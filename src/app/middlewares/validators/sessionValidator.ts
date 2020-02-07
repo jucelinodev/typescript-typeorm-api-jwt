c44a8cbb9cf7323d5608170ced5c3675d40061cb
Jucelino Novais <jucelinodev@gmail.com>
2020-02-07 16:56:54 -0300

    creates session data validation

---
+++ src/app/middlewares/validators/sessionValidator.ts
@@ -0,0 +1,23 @@
+import * as Yup from 'yup'
+import { Request, Response, NextFunction } from 'express'
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6)
+    })
+
+    await schema.validate(req.body, { abortEarly: false })
+
+    return next()
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ error: 'Validation fails', messages: err.inner[0].message })
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -4,10 +4,11 @@ import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 
 import userValidator from './app/middlewares/validators/userValidator'
+import sessionValidator from './app/middlewares/validators/sessionValidator'
 
 const router: Router = Router()
 
 router.post('/', userValidator, UserController.store)
-router.post('/session', SessionController.store)
+router.post('/session', sessionValidator, SessionController.store)
 
 export default router
