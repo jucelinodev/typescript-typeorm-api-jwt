99e57a8f402a09318fe9ba98470d8ad3ffdaf7ea
Jucelino Novais <jucelinodev@gmail.com>
2020-02-07 16:24:39 -0300

    creates user data validation

--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express'
 
 import UserController from './app/controllers/UserController'
 
+import userValidator from './app/middlewares/validators/userValidator'
+
 const router: Router = Router()
 
-router.post('/', UserController.store)
+router.post('/', userValidator, UserController.store)
 
 export default router
